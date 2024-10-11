32a98a6ecc153390db6670b6b2ecf593662f0f39
kunaldoliya90 <kunal.doliya90@gmail.com>
2024-10-11 14:38:00 +0530

    step:2 added router from express

---
+++ routes/user.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const Router = express.Router;
+// const { Router } = require("express");
+
+const userRouter = Router();
+userRouter.post("/signup", function (req, res) {
+  res.json({
+    message: "Signup Endpoint",
+  });
+});
+
+userRouter.post("/signin", function (req, res) {
+  res.json({
+    message: "Signin Endpoint",
+  });
+});
+
+userRouter.get("/purchases", function (req, res) {
+  res.json({
+    message: "Purchased Courses Endpoint",
+  });
+});
+
+module.exports = {
+  userRouter: userRouter,
+};
